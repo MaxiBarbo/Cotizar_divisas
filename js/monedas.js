89a45c25f1f816220e64b6c9b25e7349680d85f7
maxi barbo <maxi10lb@gmail.com>
2022-02-01 23:36:53 -0300

    divisas.html

--- js/monedas.js
+++ js/monedas.js
@@ -9,6 +9,9 @@
     function division(a,b){
         return a/b;
     } 
+    function multi(n1, n2) {
+        return (parseInt(n1) * parseInt(n2));
+    }
      
     function obtenerDatos(){
 
@@ -32,11 +35,11 @@
               
             let cotizacionBtc = $(".boxBtc");
             cotizacionBtc.append(`<span class="sb">1 Bitcoin <span class="iconify" data-icon="cib:btc" style="color: #ffd23f;" data-width="15" data-height="15"></span></span>
-                                  <span>$ ${datos.bpi.USD.rate_float} ${datos.bpi.USD.code}</span>`);
+                                  <span><span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="14" data-height="14"></span> ${datos.bpi.USD.rate_float}</span>`);
 
             let resultadoUno = $("#resultadoCripto");
             resultadoUno.append(`<span class="sb">Cripto:</span>
-                                <li> ${datos.chartName} | <span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="14" data-height="14"></span> ${resultadoDolar}</li>`);
+                                <li> ${datos.chartName} | <span class="iconify" data-icon="cib:btc" style="color: #ffd23f;" data-width="14" data-height="14"></span> ${resultadoDolar}</li>`);
 
             let resultadoDos = $("#resultadoFecha");
             resultadoDos.append(`<span class="sb">Fecha / Horario:</span>
@@ -48,6 +51,7 @@
 
 //Declaramos la url del archivo JSON local
 const URLJSON = 'https://api.coindesk.com/v1/bpi/currentprice.json' 
+
    
 //Accedemos el evento click del botón agregado
 $("#euro").click(() => { 
@@ -59,6 +63,7 @@ $.getJSON(URLJSON, function (respuesta, estado) {
         
       const misDatos = respuesta;
       
+
       let apiEuro = misDatos.bpi.EUR.rate_float;
       let resultadoEuro = division(pesosBtc,apiEuro);
       
@@ -71,12 +76,60 @@ $.getJSON(URLJSON, function (respuesta, estado) {
                             <li>${misDatos.time.updated.substr(0, 22)}</li>`);
 
       let cotizacionBtc = $(".boxBtc");
-      cotizacionBtc.append(`<span class="sb">1 Bitcoin<span class="iconify" data-icon="cib:btc" style="color: #ffd23f;" data-width="15" data-height="15"></span></span>
-                            <span>$ ${misDatos.bpi.EUR.rate_float} ${misDatos.bpi.EUR.code}</span>`);
+      cotizacionBtc.append(`<span class="sb">1 Bitcoin<span class="iconify" data-icon="cib:btc" style="color: #ffd23f;" data-width="14" data-height="14"></span></span>
+                            <span><span class="iconify" data-icon="el:eur" style="color: #ffd23f;" data-width="13" data-height="13"></span> ${misDatos.bpi.EUR.rate_float}</span>`);
 
     }
   });
 });
 
+const urlDolar = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
 
+
+$("#dolar").click(() => { 
+
+    let pesosDolar = $("[name*='usdArs']").val();  
+
+    $.getJSON(urlDolar, function (respuesta, estado) {
+    
+        if(estado === "success"){
+              
+          const misDatosDolar = respuesta;
+          console.log(misDatosDolar);
+          
+          const dolarBlueCompra = misDatosDolar[1].casa.compra;
+          const dolarBlueventa = misDatosDolar[1].casa.venta;
+          const dolarBlueNombre = misDatosDolar[1].casa.nombre;
+          
+          let dolarCotizar = multi(dolarBlueventa,pesosDolar);
+          let dolarCotizarOficial = multi(misDatosDolar[0].casa.venta,pesosDolar);
+          
+          
+//Seccion Dolar Blue
+
+            let cotizacionDolarBlue = $(".boxDolarBlue");
+            cotizacionDolarBlue.append(`<span><span class="sb">Compra: </span>${dolarBlueCompra}</span>
+                                        <span><span class="sb">Venta: </span>${dolarBlueventa}</span>`);
+
+            let tituloDolarBlue = $(".spanDolarBlue");  
+            tituloDolarBlue.append(`<span class="tDivisas">${dolarBlueNombre}</span>`); 
+
+            let resultadoDolarBlue = $("#resultadoDolar");
+            resultadoDolarBlue.append(`<span class="sb">Divisa:</span>
+                                <li>${misDatosDolar[0].casa.nombre}  |$ ${dolarCotizarOficial}</li>
+                                <li>${dolarBlueNombre}  |$ ${dolarCotizar}</li>`);
+
+ // Seccion Dolar Oficial  
+
+            let cotizacionDolarOficial = $(".boxDolarOficial");
+            cotizacionDolarOficial.append(`<span><span class="sb">Compra: </span>${misDatosDolar[0].casa.compra}</span>
+                                          <span><span class="sb">Venta: </span>${misDatosDolar[0].casa.venta}</span>`); 
+
+            let tituloDolarOficial = $(".spanDolarOficial");  
+            tituloDolarOficial.append(`<span class="tDivisas">${misDatosDolar[0].casa.nombre}</span>`); 
+            
+            
                             
+        }
+    });
+  });
