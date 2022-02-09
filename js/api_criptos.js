05ec6673e2bef1c558022748a46285654c4ee880
maxi barbo <maxi10lb@gmail.com>
2022-02-09 15:45:12 -0300

    divisas.html

--- js/api_criptos.js
+++ js/api_criptos.js
@@ -23,6 +23,12 @@ $("#eth").click(function(){
 
 });
 
+// $("#bnb").click(function(){
+
+//     obtenerDatos('eos','1 BNB','letsbit','BNB','usd','<span class="iconify" data-icon="cib:ethereum" style="color: #ffd23f;" data-width="15" data-height="15"></span>');
+
+// });
+
 
 function division(a,b){
     return a/b;
@@ -36,7 +42,7 @@ function multi(n1, n2) {
 function obtenerDatos(coin,titulo,exchange,cripto,fiat,icono){
 
 
-    const urlArgenBtc = `https://criptoya.com/api/${exchange}/${coin}/${fiat}/0.5`;
+    const urlArgenBtc = `https://criptoya.com/api/${exchange}/${coin}/${fiat}/0.1`;
 
     const api = new XMLHttpRequest();
     api.open('get', urlArgenBtc, true);
@@ -70,18 +76,18 @@ function obtenerDatos(coin,titulo,exchange,cripto,fiat,icono){
 // Funcion para consultar api en EURO
 
 
-const urlCriptoYa = `https://criptoya.com/api`;
+// const urlCriptoYa = `https://criptoya.com/api`;
 
 
-$("#criptoYa").click(() => { 
+// $("#criptoYa").click(() => { 
 
-    let pesosEuro = $("[name*='']").val();  
+//     let pesosEuro = $("[name*='']").val();  
 
-    $.getJSON(urlEuro, function (respuesta, estado) {
+//     $.getJSON(urlEuro, function (respuesta, estado) {
     
-        if(estado === "success"){
-            console.log(respuesta);
+//         if(estado === "success"){
+//             console.log(respuesta);
 
-        }
-    })
-});
+//         }
+//     })
+// });
