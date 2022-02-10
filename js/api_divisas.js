64ee4e4f8c0df454b7db81e7d636e0a4cfc98fc1
maxi barbo <maxi10lb@gmail.com>
2022-02-10 17:47:59 -0300

    conversion en ars divisas

--- js/api_divisas.js
+++ js/api_divisas.js
@@ -5,6 +5,7 @@ const urlDolar = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
 $("#dolar").click(() => { 
 
     let pesosDolar = $("[name*='usdArs']").val();  
+    let pesosPesos = $("[name*='ArsArs']").val();
 
     $.getJSON(urlDolar, function (respuesta, estado) {
     
@@ -17,8 +18,11 @@ $("#dolar").click(() => {
           const dolarBlueventa = misDatosDolar[1].casa.venta;
           const dolarBlueNombre = misDatosDolar[1].casa.nombre;
           
-          let dolarCotizarBlue = multi(parseFloat(dolarBlueventa),pesosDolar);
-          let dolarCotizarOficial = multi(parseFloat(misDatosDolar[0].casa.venta),pesosDolar);
+          let dolarCotizarBlue = multi(pesosDolar,parseFloat(dolarBlueventa));
+          let dolarCotizarOficial = multi(pesosDolar,parseFloat(misDatosDolar[0].casa.venta));
+
+          let pesosCotizarBlue = division(pesosPesos,parseFloat(dolarBlueventa));
+          let pesosCotizarOficial = division(pesosPesos,parseFloat(misDatosDolar[0].casa.venta));
 
        
 //Seccion Dolar Blue
@@ -29,9 +33,13 @@ $("#dolar").click(() => {
                                         <span><span class="sb">Venta: </span><span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span>${dolarBlueventa}</span>`);
 
             let resultadoDolarBlue = $("#resultadoDolar");
-            resultadoDolarBlue.append(`<span class="sb">Conversion en <span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span>ARS:</span>
-                                <li><span class="tDivisas">${misDatosDolar[0].casa.nombre}</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ${dolarCotizarOficial}</li>
-                                <li><span class="tDivisas">${dolarBlueNombre}</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ${dolarCotizarBlue}</li>`);
+            resultadoDolarBlue.append(`<span class="sb">De <span class="iconify" data-icon="emojione-v1:flag-for-united-states" data-width="25" data-height="25"></span><span class="iconify" data-icon="eva:arrow-right-outline" data-width="15" data-height="15"></span><span class="iconify" data-icon="emojione-v1:flag-for-argentina" data-width="25" data-height="25"></span> :</span>
+                                        <li><span class="tDivisas">${misDatosDolar[0].casa.nombre}</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ${dolarCotizarOficial}</li>
+                                        <li><span class="tDivisas">${dolarBlueNombre}</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ${dolarCotizarBlue}</li>
+
+                                        <span class="sb">De <span class="iconify" data-icon="emojione-v1:flag-for-argentina" data-width="25" data-height="25"></span><span class="iconify" data-icon="eva:arrow-right-outline" data-width="15" data-height="15"></span><span class="iconify" data-icon="emojione-v1:flag-for-united-states" data-width="25" data-height="25"></span> :</span>
+                                        <li><span class="tDivisas">${misDatosDolar[0].casa.nombre}</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ${pesosCotizarOficial} </li>
+                                        <li><span class="tDivisas">${dolarBlueNombre}</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ${pesosCotizarBlue}</li>`);
 
  // Seccion Dolar Oficial  
 
@@ -53,6 +61,7 @@ $("#dolar").click(() => {
 $("#eur").click(() => { 
 
     let pesosEuro = $("[name*='usdArs']").val();  
+    let pesosPesos = $("[name*='ArsArs']").val();
 
     $.getJSON(urlEuro, function (respuesta, estado) {
     
@@ -71,7 +80,7 @@ $("#eur").click(() => {
                                         <span><span class="sb">Compra: </span><span class="iconify" data-icon="el:eur" style="color: #ffd23f;" data-width="14" data-height="14"></span>${euroBlueCompra}</span>
                                         <span><span class="sb">Venta: </span><span class="iconify" data-icon="el:eur" style="color: #ffd23f;" data-width="14" data-height="14"></span>${euroBlueventa}</span>`);
 
-            // Seccion Dolar Oficial  
+            // Seccion Euro Oficial  
 
             let cotizacionEuroOficial = $(".boxDolarOficial");
             cotizacionEuroOficial.append(`<span class="tDivisas">Euro Oficial</span>
@@ -79,13 +88,20 @@ $("#eur").click(() => {
                                           <span><span class="sb">Venta: </span><span class="iconify" data-icon="el:eur" style="color: #ffd23f;" data-width="14" data-height="14"></span>${misDatosEuro.oficial_euro.value_buy}</span>`); 
 
             
-            let euroCotizarBlue = multi(parseFloat(euroBlueventa),pesosEuro);
-            let euroCotizarOficial = multi(parseFloat(misDatosEuro.oficial_euro.value_buy),pesosEuro);
+            let euroCotizarBlue = multi(pesosEuro,parseFloat(euroBlueventa));
+            let euroCotizarOficial = multi(pesosEuro,parseFloat(misDatosEuro.oficial_euro.value_buy));
+
+            let pesosEuroCotizarBlue = division(pesosPesos,parseFloat(euroBlueventa));
+            let pesosEuroCotizarOficial = division(pesosPesos,parseFloat(misDatosEuro.oficial_euro.value_buy));
 
             let resultadoEuroBlue = $("#resultadoDolar");
-            resultadoEuroBlue.append(`<span class="sb">Conversion en <span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span> ARS:</span>
-                                <li>Euro Oficial |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span></span> ${euroCotizarOficial}</li>
-                                <li>Euro Blue |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span></span> ${euroCotizarBlue}</li>`);
+            resultadoEuroBlue.append(`<span class="sb">De <span class="iconify" data-icon="twemoji:flag-for-flag-european-union" data-width="25" data-height="25"></span><span class="iconify" data-icon="eva:arrow-right-outline" data-width="15" data-height="15"></span><span class="iconify" data-icon="emojione-v1:flag-for-argentina" data-width="25" data-height="25"></span> :</span>
+                                <li><span class="tDivisas">Euro Oficial</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span></span> ${euroCotizarOficial}</li>
+                                <li><span class="tDivisas">Euro Blue</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span></span> ${euroCotizarBlue}</li>
+                                
+                                <span class="sb">De <span class="iconify" data-icon="emojione-v1:flag-for-argentina" data-width="25" data-height="25"></span><span class="iconify" data-icon="eva:arrow-right-outline" data-width="15" data-height="15"></span><span class="iconify" data-icon="twemoji:flag-for-flag-european-union" data-width="25" data-height="25"></span> :</span>
+                                <li><span class="tDivisas">Euro Oficial</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span></span> ${pesosEuroCotizarOficial}</li>
+                                <li><span class="tDivisas">Euro Blue</span> |<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="15" data-height="15"></span></span> ${pesosEuroCotizarBlue}</li>`);
 
         }
     })
