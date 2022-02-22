d244ad2fac9625071e17257ca2e9313895b2c129
maxi barbo <maxi10lb@gmail.com>
2022-02-22 00:14:59 -0300

    nueva api

--- js/api_divisas.js
+++ js/api_divisas.js
@@ -160,40 +160,49 @@ function apiEuro(icono){
 // FUncion para ingresar en Api con Key
   
   $("#usdEuro").click(() => {
-      datosApiDivisas('<span class="iconify" data-icon="emojione-v1:flag-for-united-states" style="color: #ffd23f;" data-width="30" data-height="30"></span>','<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="20" data-height="20"></span>');
+      datosApiDivisas('<span class="iconify" data-icon="emojione-v1:flag-for-united-states" style="color: #ffd23f;" data-width="30" data-height="30"></span>','<span class="iconify" data-icon="el:usd" style="color: #ffd23f;" data-width="20" data-height="20"></span>')
+      
   });
+
 
 
   //api www.freeCurrency.com
   // const apiKey = "8a132e310-9324-11ec-9a68-75030c11c4ff";
   // const urltrendingFree = 'https://freecurrencyapi.net/api/v2/latest?apikey=a132e310-9324-11ec-9a68-75030c11c4ff';
-  
   // api de www.Fixer.com
   const urltrendingFixer = 'http://data.fixer.io/api/latest?access_key=3ccedb265dbf15a3f8ae9bcccb7c5ab3'; 
   const urlCurr = 'https://free.currconv.com/api/v7/currencies??apiKey=ebf2143557128efbca59'
   const urlcurrJson = 'https://free.currconv.com/api/v7/convert?q=USD_EUR,EUR_USD&compact=ultra&callback=sampleCallback&apiKey=ebf2143557128efbca59'
 
+  endpoint = 'latest'
+  acces_key = '3ccedb265dbf15a3f8ae9bcccb7c5ab3';
+
 function datosApiDivisas(flag,fiat){   
 
   let dolar = $("[name*='usdArs']").val();
-  // let url_gif = urltrending + "?api_key=" + apiKey;
 
       $.ajax({
           method: "GET",
-          url: urltrendingFixer,
+          url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + acces_key,
           success: function (respuesta) {
            
-console.log(respuesta);
+console.log(respuesta.rates);
 
-              let usd_euro = respuesta.rates.USD;
+              let usd_euro = respuesta.rates;
               let resultado = multi(dolar,usd_euro)
 console.log(usd_euro)   
 
             $("#table").prepend(`                    
               <tr>
                 <th class="sb">${flag}</th>
-                <td class="sb">${fiat}</td>
-                <td class="sb">${fiat}${usd_euro}</td>
+                <td class="sb">$</td>
+                <td class="sb">${fiat}${usd_euro.USD}</td>
+                <td class="sb">%</td>
+              </tr>
+              <tr>
+                <th class="sb"><span class="iconify" data-icon="noto-v1:flag-for-flag-united-kingdom" style="color: silver;" data-width="30" data-height="30"></span></th>
+                <td class="sb">$</td>
+                <td class="sb"><span class="iconify" data-icon="el:gbp" style="color: #ffd23f;" data-width="20" data-height="20"></span>${usd_euro.GBP}</td>
                 <td class="sb">%</td>
               </tr> `)
               if(dolar !==''){
